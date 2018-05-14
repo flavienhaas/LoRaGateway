c822463ef97ec3c640dcd43b795a7d65ff4da2e9
Flavien Haas <flavorita.hasso@gmail.com>
2018-05-14 13:05:59 +0200

    add the rest of the frame

--- fakeStation/fakeStation.ino
+++ fakeStation/fakeStation.ino
@@ -21,7 +21,7 @@ void setup() {
   message.ID = 0x0007;
   message.TS = 0x0001;
   message.DT = 0x0001;
-  message.D1 = 0x0002;
+  message.D1 = 0x0026;
   message.D2 = 0x0003;
   message.D3 = 0x0004;
 
@@ -38,6 +38,16 @@ void loop() {
   LoRa.beginPacket();
   LoRa.write((uint8_t)(message.ID >> 8));
   LoRa.write((uint8_t)message.ID);
+  LoRa.write((uint8_t)(message.TS >> 8));
+  LoRa.write((uint8_t)message.TS);
+  LoRa.write((uint8_t)(message.DT >> 8));
+  LoRa.write((uint8_t)message.DT);
+  LoRa.write((uint8_t)(message.D1 >> 8));
+  LoRa.write((uint8_t)message.D1);
+  LoRa.write((uint8_t)(message.D2 >> 8));
+  LoRa.write((uint8_t)message.D2);
+  LoRa.write((uint8_t)(message.D3 >> 8));
+  LoRa.write((uint8_t)message.D3);
   LoRa.endPacket();
 
   delay(3000);
